7263a812c4330a9948daf7dfda97c19f0a9ba90d
Charles Acquah-Davis <c.acquahdavis@gmail.com>
2026-02-04 23:14:25 +0000

    Clean repo structure and remove unused backend files

--- new-backend/server/dist/stripe.js
+++
@@ -1,4 +0,0 @@
-import Stripe from 'stripe';
-import { env } from './utils.js';
-export const stripe = new Stripe(env('STRIPE_SECRET_KEY'), { apiVersion: '2023-10-16' });
-//# sourceMappingURL=stripe.js.map
--- new-backend/server/src/stripe.ts
+++
@@ -1,3 +0,0 @@
-import Stripe from 'stripe';
-import { env } from './utils.js';
-export const stripe = new Stripe(env('STRIPE_SECRET_KEY'), { apiVersion: '2023-10-16' });
